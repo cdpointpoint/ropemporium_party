e1ca61f50c8df38f2d985c0e45634127d79dcdd4
cdpointpoint <cdpointpoint@gmail.com>
2025-06-15 15:32:01 +0200

    run latest image

--- run_ptsrew.sh
+++ run_ptsrew.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 # run the workshop docker image
+# the current challege directory is mounted as /share in the docker image
+# and linked with /home/pts/challenge 
 docker run -it --rm  -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix   \
-  --name pts_rew -v $(pwd)/challenges:/share cdpointpoint/ropemporium:0.2 /usr/bin/tmux
+  --name pts_rew -v $(pwd)/challenges:/share cdpointpoint/ropemporium /usr/bin/tmux
 # --user $(id -u):$(id -u) \
